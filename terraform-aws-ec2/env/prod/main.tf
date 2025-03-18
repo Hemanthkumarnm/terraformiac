db86542029d4b7f64c55e4ca41ed13412ced6694
Hemanth kumar N M <142966959+Hemanthkumarnm@users.noreply.github.com>
2025-03-18 15:47:23 +0530

    Update main.tf

--- terraform-aws-ec2/env/prod/main.tf
+++ terraform-aws-ec2/env/prod/main.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source         = "../../modules/ec2-instance"
   ami           = "ami-12345678"   # Replace with a valid AMI ID
   instance_type = "t2.micro"
-  subnet_id     = "subnet-abcdef"  # Replace with a valid Subnet ID
+  #subnet_id     = "subnet-abcdef"  # Replace with a valid Subnet ID
   key_name      = "my-key"
   instance_name = "dev-instance"
 }
