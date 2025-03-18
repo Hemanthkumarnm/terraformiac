d62d2f03c010b76ee633ac08148d036afd5acc6e
Hemanth kumar N M <142966959+Hemanthkumarnm@users.noreply.github.com>
2025-03-18 15:51:55 +0530

    Update main.tf

--- terraform-aws-ec2/env/prod/main.tf
+++ terraform-aws-ec2/env/prod/main.tf
@@ -3,7 +3,7 @@ module "ec2_instance" {
   ami           = "ami-00bb6a80f01f03502"   
   instance_type = "t2.micro"
   #subnet_id     = "subnet-abcdef"  # Replace with a valid Subnet ID
-  key_name      = "my-key"
+  key_name      = "my-jenkins-key"
   instance_name = "jenkins-instance"
 }
 
