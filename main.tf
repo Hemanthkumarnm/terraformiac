458ea21b8bbb8677aad56ddde21ce1d1d3defebd
hemanth <hemanth.drivex@drivex.in>
2025-03-25 13:35:02 +0530

    added s3 file

--- main.tf
+++ main.tf
@@ -12,13 +12,14 @@ provider "aws" {
 }
 
 module "ec2_instances" {
-  source = "./terraform-aws-ec2"
+  source = "./terraform-aws-ec2/ec2-instance"
 
   # Required variables
   ami           = "ami-0c7217cdde317cfec"  # Ubuntu 22.04 LTS in us-east-1
   subnet_id     = "subnet-1234567890abcdef0"  # Replace with your subnet ID
   key_name      = "your-key-pair-name"  # Replace with your key pair name
   instance_name = "web-server"
+  allowed_ssh_ip = "0.0.0.0/0"  # Replace with your IP address for SSH access
 
   # Optional variables
   instance_type = "t2.micro"
@@ -32,4 +33,4 @@ module "ec2_instances" {
     Environment = "development"
     Project     = "my-project"
   }
-} 
+}
