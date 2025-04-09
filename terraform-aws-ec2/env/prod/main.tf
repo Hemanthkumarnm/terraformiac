1d3600c6aa22605e39ba18e61650a2562a7d4aa3
Hemanth kumar N M <142966959+Hemanthkumarnm@users.noreply.github.com>
2025-04-09 11:18:00 +0530

    Delete terraform-aws-ec2 directory

--- terraform-aws-ec2/env/prod/main.tf
+++
@@ -1,9 +0,0 @@
-module "ec2_instance" {
-  source         = "../../modules/ec2-instance"
-  ami           = "ami-00bb6a80f01f03502"   
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-0e61cba69846da910 "  # Replace with a valid Subnet ID
-  key_name      = "my-jenkins-key"
-  instance_name = "jenkins-instance"
-}
-
--- terraform-aws-ec2/env/prod/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-
-ami = "ami-00bb6a80f01f03502"
-instance_type = "t2.micro"
-#subnet_id = "subnet-abcdef"
-key_name = "my-key"
-instance_name = "dev-instance"
